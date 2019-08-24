c06f52fceddd63888ddd15708577b071a9d3e657
Diego Monroy <difemo91@gmail.com>
2019-08-24 05:04:19 +0000

    [FEATURE] add functions to handle some built-ins

--- exec.c
+++ exec.c
@@ -8,14 +8,33 @@
 
 int execute(char **parse)
 {
+	int i = 0;
 	pid_t pid;
 	int status;
+	char *ptr;
+
+	builtin_t list[] = {
+		{"help", _help},
+		{"exit", my_exit},
+		{NULL, NULL}
+	};
+
+	while (list[i].s != NULL && parse[0] != NULL)
+	{
+		ptr = list[i].s;
+		if(_strcmp(parse[0], ptr) == 0)
+		{
+			list[i].f();
+			return (0);
+		}
+		i++;
+	}
 
 	pid = fork();
 	if (pid == 0)
 	{
 		if (execve(parse[0], parse, NULL) == -1)
-			perror("Error:");
+			perror("Error");
 		exit(1);
 	}
 	else if (pid > 0)
@@ -26,4 +45,17 @@ int execute(char **parse)
 		perror("Error:");
 
 	return (0);
+}
+
+void _help()
+{
+	_puts("Welcome to the simple shell help\n");
+	_puts("Type something and hit enter\n");
+	_puts("You can use exit to close interactive mode\n");
+}
+
+void my_exit()
+{
+	_puts("Have a nice day!\n");
+	exit(1);
 }
--- holberton.h
+++ holberton.h
@@ -6,9 +6,24 @@
 #include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+/**
+ * struct optype - struct for built in commands
+ *
+ * @s: string
+ * @f: pointer to function
+ */
+typedef struct builtin
+{
+	char *s;
+	void (*f)(void);
+}builtin_t;
 char *read_line(void);
 char **splits(char *line);
 int execute(char **parse);
 void _puts(char *str);
 int _putchar (char c);
+char *_strcat(char *dest, char *src);
+int _strcmp(char *, char *);
+void _help(void);
+void my_exit(void);
 #endif
